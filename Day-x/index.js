36c0987c2b9d3f99623ffcf42f1dc9b1493f1953
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-08-11 21:15:24 +0530

    updates

---
+++ Day-x/index.js
@@ -0,0 +1,120 @@
+Data types 
+
+
+1. String : 
+
+Syntax = "", '', ``
+Examples : 
+"Swaraj", 'jadhavswaraj@gmail.com', `34` , "My name is awdiz",
+'My age is 34 and my school is awdiz..'
+
+2. Number : 
+
+Syntax : 
+34, 54678, -9876, 23.45, 0
+
+3 Boolean : 
+
+Syntax : 
+true , false
+
+4. Undefined
+
+Initially no value 
+
+
+
+
+Varibles - 
+
+var 
+let 
+const
+
+var : 
+
+syntax : var nameOFvarible = value ;
+
+var myName = "Swaraj";
+var mySurname = "Jadhav";
+var myAge = 87;
+var isCompletedAssignment = false;
+var kuchBhi = "Kuchbhi"
+var description = "My name is swaraj im from mumbai im working at toyow..."
+console.log(myName)
+
+console.log(myAge)
+console.log(description)
+console.log(mySurname)
+console.log(kuchBhi)
+
+
+console.log(isCompletedAssignment)
+
+
+var myName = "Awdiz";
+console.log(myName, typeof (myName))
+var mySurname = 'Classes';
+console.log(mySurname, typeof (mySurname))
+var myCity = `Mumbai`
+console.log(myCity, typeof (myCity))
+
+var myAge = 60;
+console.log(myAge , typeof(myAge))
+
+var isHadDinner = false;
+console.log(isHadDinner, typeof (isHadDinner))
+
+var myDecscription ;
+console.log(myDecscription, typeof(myDecscription))
+
+var num1 = 987;
+var num2 = 234;
+var addition = num1 + num2;
+console.log(addition, "- addition")
+var subtraction = num1 - num2;
+console.log(subtraction, "- subtraction")
+var multi = num1 * num2;
+console.log(multi, "- Multiplication");
+var divid = num1 / (num2 + num1);
+console.log(divid, "- dividation")
+
+var additionOfThree = num1 + num2 + num1;
+console.log(additionOfThree)
+
+
+
+
+
+var myName = "XYZ";
+
+var message = "Welcome To our website, Mr. ${myName}";
+
+var newMessage = `Welcome to our website Mr. ${myName}`
+
+console.log(message);
+console.log(newMessage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
