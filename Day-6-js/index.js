d3c5e582d8ca99ba87d21c1c3924bd2a0e910ec3
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-08-20 17:05:19 +0530

    uPDATES

--- Day-6-js/index.js
+++ Day-6-js/index.js
@@ -1,5 +1,5 @@
-// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 94, 23, 45, 67, 12];
-// const target = 10;
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 94, 23, 45, 67, 12];
+const target = 10;
 
 // // i j k
 // // 0 1 2
@@ -9,24 +9,24 @@
 // // 0 1 6
 // // 0 1 x
 
-// for (var i = 0; i < array.length - 2; i++) {
-//     // console.log(array[i])
-//     for (var j = i + 1; j < array.length - 1; j++) {
-//         // if (array[i] + array[j] == target) {
-//         //     console.log(array[i], array[j])
-//         // }
-//         for (var k = j + 1; k < array.length; k++) {
-//             if (array[i] + array[j] + array[k] == target) {
-//                 console.log(array[i], array[j], array[k])
-//             }
-//             // console.log(i, j, k)
-//             // k++  - 2 - 3 - 4 -5 - 6 -7 -8
-//             // k 3
-//         }
-//         // j++ 1- 2
-//     }
-//     // i++
-// }
+for (var i = 0; i < array.length - 2; i++) {
+    // console.log(array[i])
+    for (var j = i + 1; j < array.length - 1; j++) {
+        // if (array[i] + array[j] == target) {
+        //     console.log(array[i], array[j])
+        // }
+        for (var k = j + 1; k < array.length; k++) {
+            if (array[i] + array[j] + array[k] == target) {
+                console.log(array[i], array[j], array[k])
+            }
+            // console.log(i, j, k)
+            // k++  - 2 - 3 - 4 -5 - 6 -7 -8
+            // k 3
+        }
+        // j++ 1- 2
+    }
+    // i++
+}
 
 
 // console.log("First")
@@ -36,89 +36,91 @@
 // console.log("5")
 
 
-// function kuchBhi() {
-//     console.log("Inside function")
-//     console.log("Inside function")
-//     console.log("Inside function")
-//     console.log("Inside function")
-//     console.log("Inside function")
-//     console.log("Inside function")
-//     console.log("Inside function")
-// }
-// kuchBhi()
+function kuchBhi() {
+    console.log("Inside function")
+    console.log("Inside function")
+    console.log("Inside function")
+    console.log("Inside function")
+    console.log("Inside function")
+    console.log("Inside function")
+    console.log("Inside function")
+}
+kuchBhi()
 
-// function addition() {
-//     var a = 34;
-//     var b = 98;
-//     // console.log(a + b)
-//     return a + b;
-// }
+function addition() {
+    var a = 34;
+    var b = 98;
+    // console.log(a + b)
+    return a + b;
+}
 
-// console.log(addition())
+console.log(addition())
 
-// function subtraction() {
-//     var a = 98;
-//     var b = 98;
-//     return a - b;
-// }
+function subtraction() {
+    var a = 98;
+    var b = 98;
+    return a - b;
+}
 
-// console.log(subtraction())
-
-
-// function multi(firstNumber, secondNumber, third) {
-//     console.log(firstNumber, secondNumber, third)
-//     const result = firstNumber * secondNumber;
-//     const result2 = firstNumber + secondNumber + third;
-//     return result + result2;
-// }
-// console.log(multi(23, 39876, 76));
+console.log(subtraction())
 
 
-// function Welcome(name, surname, city, age) {
-//     console.log(`I'm ${name} my surname is ${surname} and im from ${city} and my age is ${age}`)
-// }
-// Welcome("Rahul", "Surname", "Pune", 87)
+function multi(firstNumber, secondNumber, third) {
+    console.log(firstNumber, secondNumber, third)
+    const result = firstNumber * secondNumber;
+    const result2 = firstNumber + secondNumber + third;
+    return result + result2;
+}
+console.log(multi(23, 39876, 76));
+
+
+function Welcome(name, surname, city, age) {
+    console.log(`I'm ${name} my surname is ${surname} and im from ${city} and my age is ${age}`)
+}
+Welcome("Rahul", "Surname", "Pune", 87)
 
 
 
-// var array = [1, 2, 4, 6, 7, 8, 0, 12, 34, 54, 76];
+var array = [1, 2, 4, 6, 7, 8, 0, 12, 34, 54, 76];
 
-// function additionOfAllElement(array) {
-//     // console.log(array,"- array")
-//     var result = 0;
-//     for (var i = 0; i < array.length; i++) {
-//         result = result + array[i]
-//     }
-//     return result ;
-// }
+function additionOfAllElement(array) {
+    // console.log(array,"- array")
+    var result = 0;
+    for (var i = 0; i < array.length; i++) {
+        result = result + array[i]
+    }
+    return result;
+}
 
-// console.log(additionOfAllElement(array))
-
-
-// var myArray = [32, 34, 56, 67, 78, 34, 65, 52, 73, 23, 12];
-
-// var target = 23;
-
-// function find(array, target) {
-//     // var gotIt = false;
-
-//     for (var i = 0; i < array.length; i++) {
-//         if (array[i] == target) {
-//             // gotIt = true;
-//             return "Got it"; // function stop
-//         }
-//     }
-
-//     // if (gotIt == false) {
-//     return "No"
-//     // }
-
-// }
-
-// console.log(find(myArray, target))
+console.log(additionOfAllElement(array))
 
 
-var myArray = [32, 34, 56, 67, 78, 34, 65, 52, 73, 23, 23,23, 12];
+var myArray = [32, 34, 56, 67, 78, 34, 65, 52, 73, 23, 12];
+
+var target = 23;
+
+function find(array, target) {
+    // var gotIt = false;
+
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == target) {
+            // gotIt = true;
+            return "Got it"; // function stop
+        }
+    }
+
+    // if (gotIt == false) {
+    return "No"
+    // } else {
+    // return "false"
+    // }
+
+}
+
+console.log(find(myArray, target))
+
+
+var myArray = [32, 34, 56, 67, 78, 34, 65, 52, 73, 23, 23, 23, 12];
 
 var target = 23;
 
---
+++ day-7-js/index.js
@@ -0,0 +1,53 @@
+// Q Given an array 
+
+// array
+
+// var array = ["adw", 23, true]
+
+
+// Object
+
+// var kuchbhi =
+//     { key: "value" }
+// JSON
+// { "key" : "value" }
+
+// var kuchbhi = { name: "Awdiz", age: 5, noOfDays: ["Monday", { day: "Tuesday" }], isOpen: true, myData: { hi: "hello", myArray: [1, 2, 4, 5, 6,987] } }
+
+
+// console.log(kuchbhi)
+// console.log(kuchbhi.name)
+// console.log(kuchbhi.noOfDays[0])
+// console.log(kuchbhi.noOfDays[1].day)
+// console.log(kuchbhi.myData.hi)
+// console.log(kuchbhi.myData.myArray)
+// console.log(kuchbhi.myData.myArray[kuchbhi.myData.myArray.length - 1])
+
+
+// JSON 
+
+// js to json = code to browser = JSON.stringify(localStorage.setItem({ key: "value" }))
+// json to js = browser to code = JSON.parse(localStorage.getItem("name"))
+
+
+// localStorage
+
+// // save to ls
+// localStorage.setItem(key, value)
+
+// // read from ls
+// localStorage.getItem(key)
+// const mydata = localStorage.getItem("name")
+
+// localStorage.removeItem(JSON.stringify(key))
+
+
+const array = [];
+
+array.push("ytguhij")
+array.push("abc")
+array.push("xyz")
+array.push(87654)
+array.push({ hi: "hello" })
+
+console.log(array, "- array")
